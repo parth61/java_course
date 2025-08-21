19b95df64b5b5d526b3614b4fb32bb3edaa1fdca
parth.ramani <rparth61@gmail.com>
2025-08-21 18:48:21 +0530

    first commit

---
+++ src/main/java/chapter4/GrossPayInputValidation.java
@@ -0,0 +1,8 @@
+package chapter4;
+
+public class GrossPayInputValidation {
+    public static void main(String[] args) {
+        System.out.println("Enter number of hours employee worked");
+
+    }
+}
