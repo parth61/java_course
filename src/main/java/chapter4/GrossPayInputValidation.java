dcd19c6dd14de7217692361baf69116815605db0
parth.ramani <rparth61@gmail.com>
2025-08-22 12:15:19 +0530

    first commit

--- src/main/java/chapter4/GrossPayInputValidation.java
+++ src/main/java/chapter4/GrossPayInputValidation.java
@@ -1,8 +1,21 @@
 package chapter4;
 
+import java.util.Scanner;
+
 public class GrossPayInputValidation {
     public static void main(String[] args) {
-        System.out.println("Enter number of hours employee worked");
+        int rate = 15;
+        int maxHours = 40;
+        Scanner input = new Scanner(System.in);
+        double hoursWorked = input.nextDouble();
+        while (hoursWorked > maxHours || hoursWorked < 1) {
+            System.out.println("Hours worded should be between 1 to 40");
+            hoursWorked = input.nextDouble();
+
+        }
+        input.close();
+        double gross = rate*hoursWorked;
+        System.out.println("Gross Pay : "+ gross);
 
     }
 }
