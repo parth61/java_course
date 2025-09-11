4d7908f951487877dd449581a23720c09c96e4df
parth.ramani <rparth61@gmail.com>
2025-09-11 20:14:00 +0530

    first commit

---
+++ src/main/java/chapter6/PhoneBillCalculator.java
@@ -0,0 +1,11 @@
+package chapter6;
+
+public class PhoneBillCalculator {
+    public static void main(String[] args) {
+        PhoneBill bill = new PhoneBill();
+        bill.setMinutesUsed(1000);
+        bill.printItemizedBill();
+
+
+    }
+}
